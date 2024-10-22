49eb931ae13bad5b0f65fe66880a7038697b89a3
Roger Costa <roger.desenv@gmail.com>
2024-10-21 23:08:07 -0300

    feito

---
+++ DesafioControleFluxo/src/Exercicio04.java
@@ -0,0 +1,10 @@
+public class Exercicio04 {
+  public static void main(String[] args) {
+    // Criando um array de inteiros
+    int[] numeros = {10, 20, 30, 40, 50};
+
+    for (int numero : numeros ) {
+      System.out.println("O número do array é: " + numero);
+    }
+  }
+}
