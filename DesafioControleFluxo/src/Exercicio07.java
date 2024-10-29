ce062ba63672c9bb9bf4407a9fbbc4264db934d4
Roger Costa <roger.desenv@gmail.com>
2024-10-29 20:54:30 -0300

    aprendendo java

---
+++ DesafioControleFluxo/src/Exercicio07.java
@@ -0,0 +1,20 @@
+public class Exercicio07 {
+  public static void main(String[] args) {
+    
+    int numero1 = 10;
+    int numero2 = 5;
+    int multiplicacao = numero1 * numero2;
+    int soma = numero1 + numero2;
+    int subtracao = numero1 - numero2;
+    int divisao = numero1 / numero2;
+
+    System.out.println(multiplicacao);
+    
+    System.out.println(soma);
+    
+    System.out.println(subtracao);
+    
+    System.out.println(divisao);
+
+  }
+}
