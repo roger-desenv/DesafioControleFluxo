2149a18b1c2c92bc68c9ec89548664f97f0b0366
Roger Costa <roger.desenv@gmail.com>
2024-11-03 19:19:34 -0300

    finanlizando

---
+++ DesafioControleFluxo/Exercicio19.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+
+        for (int i = 0; i < 5; i++) { // Inicia o loop em i = 0
+            System.out.print("Insira o número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        System.out.println("Números inseridos: ");
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+        
+        scanner.close(); // Fecha o scanner
+    }
+}
+
--- DesafioControleFluxo/src/Exercicio17.java
+++ DesafioControleFluxo/src/Exercicio17.java
@@ -3,21 +3,27 @@ import java.util.Scanner;
 public class Exercicio17 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] numeros = new int[2]; // Declaração do array com 2 posições
+        int[] numeros = new int[5]; // Declaração do array com 5 posições
 
-        // Loop para pedir 2 números ao usuário e armazená-los no array
-        for (int i = 0; i < 2; i++) {
+        // Loop para pedir 5 números ao usuário e armazená-los no array
+        for (int i = 0; i < 5; i++) {
             System.out.print("Insira o número " + (i + 1) + ": ");
             numeros[i] = scanner.nextInt(); // Armazena o número inserido no array
         }
 
-        // Exibe os números armazenados
-        System.out.println("Os números inseridos foram:");
-        for (int numero : numeros) {
-            System.out.println(numero);
+        // Inicializa 'menor' com o primeiro elemento do array após preenchê-lo
+        int menor = numeros[0];
+
+        // Loop para encontrar o menor valor
+        for (int i = 1; i < numeros.length; i++) {
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
         }
 
-       
+        // Exibe o menor valor encontrado
+        System.out.println("O menor valor no array é: " + menor);
+        
+        scanner.close();
     }
 }
-
